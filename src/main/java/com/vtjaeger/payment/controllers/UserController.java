809a6da4603e0ce5484d4a09bf01fc27e05df839
vtjaeger <viniciustjaeger@gmail.com>
2024-08-21 16:04:15 -0300

    send email of confirmation

--- src/main/java/com/vtjaeger/payment/controllers/UserController.java
+++ src/main/java/com/vtjaeger/payment/controllers/UserController.java
@@ -4,11 +4,14 @@ import com.vtjaeger.payment.dtos.UserRequest;
 import com.vtjaeger.payment.dtos.UserResponse;
 import com.vtjaeger.payment.models.User;
 import com.vtjaeger.payment.services.UserService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,7 +22,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid UserRequest userRequest) {
+    public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid UserRequest userRequest) throws MessagingException, UnsupportedEncodingException {
         User user = userRequest.toModel();
         return ResponseEntity.ok(userService.saveUser(user));
     }
@@ -37,5 +40,14 @@ public class UserController {
         ).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(responseList);
+    }
+
+    @GetMapping("/verify")
+    public String verifyUser(@Param("code") String code){
+        if(userService.verify(code)){
+            return "verify_success";
+        } else {
+            return "verify_fail";
+        }
     }
 }
--- src/main/java/com/vtjaeger/payment/services/UserService.java
+++ src/main/java/com/vtjaeger/payment/services/UserService.java
@@ -5,11 +5,13 @@ import com.vtjaeger.payment.dtos.UserResponse;
 import com.vtjaeger.payment.models.User;
 import com.vtjaeger.payment.repositories.UserRepository;
 import com.vtjaeger.payment.util.RandomString;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @Service
@@ -18,8 +20,10 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private MailService mailService;
 
-    public UserResponse saveUser(User user) {
+    public UserResponse saveUser(User user) throws MessagingException, UnsupportedEncodingException {
         if(userRepository.findByEmail(user.getEmail()) != null) {
             throw new RuntimeException("email already exists");
         } else {
@@ -31,6 +35,7 @@ public class UserService {
             user.setEnabled(false);
 
             User savedUser = userRepository.save(user);
+            mailService.sendVerificationEmail(user);
 
             return new UserResponse(
                     savedUser.getId(),
@@ -42,5 +47,18 @@ public class UserService {
 
     public List<User> getUsers(){
         return userRepository.findAll();
+    }
+
+    public boolean verify(String verificationCode){
+        User user = userRepository.findByVerificationCode(verificationCode);
+        if(user == null || user.isEnabled()){
+            return false;
+        } else {
+            user.setVerificationCode(null);
+            user.setEnabled(true);
+            userRepository.save(user);
+
+            return true;
+        }
     }
 }
